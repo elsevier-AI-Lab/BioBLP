0504020a26eebaf200835d1a1d86bc3c1e289b3c
Thom Pijnenburg <t.pijnenburg@elsevier.com>
2022-09-15 11:15:20 +0000

    move code to module

--- bioblp/benchmarking/embeddings.py
+++ bioblp/benchmarking/embeddings.py
@@ -1,97 +1,102 @@
 import torch
 import json
+import abc
 
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Union
 
 
+@dataclass
 class LookupEmbedding():
-    def __init__(self, random_seed: int = 42, dim=128, emb_range=(-1, 1)):
 
-        self.random_seed = random_seed
-
-        self.embeddings = None
-        self.dim = dim
-        self.emb_range = emb_range
-        self.entity_to_id = {}
+    embeddings: torch.tensor
+    entity_to_id: dict
+    metadata: dict
 
     @classmethod
     def from_pretrained(cls, model_path):
-        model_path = Path(model_path)
-        cfg = {}
-        with open(model_path.joinpath("config.json"), "r") as f:
-            cfg = json.load(f)
-            
-        embeddings = torch.load(model_path.joinpath("embeddings.pt"))
-        entity_to_id = pd.read_csv(model_path.joinpath("entity_to_id.tsv"), sep="\t", index_col = None, header=None)
-        entity_to_id = {x[0]:x[1] for x in entity_to_id.values }
-        
-        obj = cls(random_seed = cfg["random_seed"],
-                 dim=cfg["dim"],
-                 emb_range=cfg["emb_range"])
-        
-        obj.embeddings = embeddings
-        obj.entity_to_id = entity_to_id
-        return obj
+        _embedding_file = "embeddings.pt"
+        _e2id_file = "entity_to_id.tsv"
+        _metadata_file = "metadata.json"
 
-    def save(self, outdir: Union[str, Path], name="noise_embeddings"):
+        model_path = Path(model_path)
+
+        required = [_embedding_file, _e2id_file]
+
+        for file_i in required:
+            assert model_path.joinpath(file_i).exists(
+            ), f"Missing required file {file_i} in dir {model_path}"
+
+        embeddings = torch.load(model_path.joinpath(_embedding_file))
+        entity_to_id = pd.read_csv(model_path.joinpath(
+            _e2id_file), sep="\t", index_col=None, header=None)
+        entity_to_id = {x[0]: x[1] for x in entity_to_id.values}
+
+        metadata = None
+        if model_path.joinpath(_metadata_file).exists():
+            with open(model_path.joinpath(_metadata_file), "r") as f:
+                metadata = json.load(f)
+
+        return cls(embeddings=embeddings, entity_to_id=entity_to_id, metadata=metadata)
+
+    def save(self, outdir: Union[str, Path], name: str):
         outdir = Path(outdir).joinpath(name)
         outdir.mkdir(parents=True, exist_ok=False)
-        
-        config = {
-            "random_seed": self.random_seed,
-            "dim": self.dim,
-            "emb_range": self.emb_range
-        }
-        
-        with open(outdir.joinpath("config.json"), "w") as f:
-            json.dump(config, f)
-        
-        df = pd.DataFrame([[k, v] for k, v in self.entity_to_id.items()], columns=["e_", "id_"])
-        df.to_csv(outdir.joinpath("entity_to_id.tsv"), sep="\t", index=False, header=False)
-        
+
+        df = pd.DataFrame(
+            [[k, v] for k, v in self.entity_to_id.items()], columns=["e_", "id_"])
+        df.to_csv(outdir.joinpath("entity_to_id.tsv"),
+                  sep="\t", index=False, header=False)
+
         torch.save(self.embeddings, outdir.joinpath("embeddings.pt"))
-        
 
-    def _init_embeddings(self):
-        raise NotImplementedError(
-            "Child class needs implementation of _init_embeddings.")
-
-    def _build_entity_to_id(self, entities):
-        entities = sorted(dpi_entities)
-        self.entity_to_id = {k: k_idx for k_idx, k in enumerate(entities)}
-
-    def __call__(self, entities: list):
-        if self.embeddings is None:
-            self._build_entity_to_id(entities)
-            self._init_embeddings()
-        else:
-            raise Error("Embedder already fit")
+        if self.metadata is not None:
+            with open(outdir.joinpath("metadata.json"), "w") as f:
+                json.dump(self.metadata, f)
 
 
-class NoiseEmbedding(LookupEmbedding):
-    def __init__(self, random_seed: int = 42, dim=128, emb_range=(-1, 1)):
-        super().__init__(random_seed=random_seed, dim=dim, emb_range=emb_range)
+def NoiseEmbedding(entities, random_seed: int = 42, dim=128, emb_range=(-1, 1)) -> LookupEmbedding:
+    entity_to_id = build_entity_to_id(entities)
 
-        self.dim = dim
-        self.emb_range = emb_range
-        self.entity_to_id = {}
+    emb_shape = (len(entities), dim)
 
-    def _init_embeddings(self):
-        emb_shape = (len(self.entity_to_id), self.dim)
+    r1 = emb_range[0]
+    r2 = emb_range[1]
 
-        r1 = self.emb_range[0]
-        r2 = self.emb_range[1]
+    g = torch.Generator()
+    g.manual_seed(random_seed)
 
-        g = torch.Generator()
-        g.manual_seed(self.random_seed)
+    embeddings = (r1 - r2) * torch.rand(emb_shape, generator=g) + r2
 
-        self.embeddings = (r1 - r2) * torch.rand(emb_shape, generator=g) + r2
+    metadata = {
+        "random_seed": random_seed,
+        "dim": dim,
+        "emb_range": emb_range
+    }
 
-    def _build_entity_to_id(self, entities):
-        entities = sorted(entities)
-        self.entity_to_id = {k: k_idx for k_idx, k in enumerate(entities)}
+    return LookupEmbedding(embeddings=embeddings,
+                           entity_to_id=entity_to_id,
+                           metadata=metadata)
 
-    def __call__(self, entities: list):
-        self._build_entity_to_id(entities)
-        self._init_embeddings()
+
+def MorganFingerpintEmbedding(mols, smiles) -> LookupEmbedding:
+    print('fsdf')
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    #
+    # WIP
+    #
+    parser = ArgumentParser(description="Run model training procedure")
+    parser.add_argument("--data_dir", type=str,
+                        help="Path to pick up data")
+    parser.add_argument("--outdir", type=str, help="Path to write output")
+    parser.add_argument("--name", type=str, help="name of embeddings")
+
+    args = parser.parse_args()
+    main(args)
