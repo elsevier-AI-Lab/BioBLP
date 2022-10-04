66e6ea891d855cfe46d7d972d2559d5badb876aa
Daniel <dfdazac@gmail.com>
2022-10-04 16:32:48 +0200

    Updating job scripts

---
+++ jobs/hetionet-complex-sweep.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=biokg-complex-sweep
+#SBATCH --output=array_%A_%a.out
+#SBATCH --error=array_%A_%a.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=40:00:00
+#SBATCH --mem=10G
+#SBATCH --partition=gpu_shared
+#SBATCH --gres=gpu:1
+
+PROJ_FOLDER=bioblp
+OUT_FOLDER=models
+
+# Copy data to scratch
+cp -r $HOME/$PROJ_FOLDER $TMPDIR
+cd $TMPDIR/$PROJ_FOLDER
+
+source activate bioblp
+
+wandb agent --count 1 discoverylab/bioblp/uvgnrmka
+
+# Keep files generated during job
+RESULTS_FOLDER=$HOME/$PROJ_FOLDER-$OUT_FOLDER
+mkdir -p $RESULTS_FOLDER
+cp -r $TMPDIR/$PROJ_FOLDER/$OUT_FOLDER/* $RESULTS_FOLDER
