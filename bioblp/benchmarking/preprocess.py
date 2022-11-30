03889f02ea23a69cb940bf0e29e3f858c9965d4e
p.mitra <p.mitra@elsevier.com>
2022-11-30 06:38:51 +0000

    nb for benchmark dpi nested cv

--- bioblp/benchmarking/preprocess.py
+++ bioblp/benchmarking/preprocess.py
@@ -64,24 +64,44 @@ def generate_negative_triples(pos_triples: TriplesFactory,
 
 
 def main(args):
+    bm_data_path = Path(args.bm_data_path)
+    kg_triples_dir = Path(args.kg_triples_dir)
+    outdir = Path(args.outdir)
+    num_negs_per_pos = args.num_negs_per_pos
+    bm_dataset_name = bm_data_path.name.split('.tsv')[0]
+
+    training_triples = TriplesFactory.from_path_binary(kg_triples_dir)
+    entity_to_id_map = training_triples.entity_to_id
+    relation_to_id_map = training_triples.relation_to_id
+
     # load the benchmark data
-    
+    bm_df = pd.read_csv(bm_data_path, sep='\t', names=[COL_SOURCE, COL_EDGE, COL_TARGET])
+
     # generate neg samples and prepare pos-neg pairs
-    
+    logger.info(f'Generating negative samples corresponding to benchmark triples')
+    pos_neg_df = prepare_dpi_samples(bm_df, 
+                                     entity_to_id_map=entity_to_id_map, 
+                                     relation_to_id_map= relation_to_id_map,
+                                     num_negs_per_pos=num_negs_per_pos)
+
     # create train-test-val splits
+    ### not required, taken care of in the nested cv script
     
     # save to disk
-    
-    pass
+    bm_postprocessed_path = outdir.joinpath(f"benchmarks/processed/{bm_dataset_name}_p2n-1-{num_negs_per_pos}.tsv")
+    logger.info(f'Writing preprocessed data to {bm_postprocessed_path}')
+    pos_neg_df.to_csv(bm_postprocessed_path, sep='\t')
+    logger.info('Done!')
 
 
 if __name__ == "__main__":
 
     parser = ArgumentParser(description="Preprocess benchmark triples (E.g. DPI data) for downstream prediction task")
-    parser.add_argument("--data_dir", type=str,
-                        help="Path to pick up data")
+    parser.add_argument("--bm_data_path", type=str,
+                        help="Path to pick up benchmark data")
+    parser.add_argument("--kg_triples_dir", type=str, help="Directory housing kg positive triples. Needed to generate negative samples")
     parser.add_argument("--num_negs_per_pos", type=int, help="Number of negative samples to generate per positive instance")
-    parser.add_argument("--outdir", type=str, help="Path to write output")
+    parser.add_argument("--outdir", type=str, help="Path to data dir to write output")
 
     args = parser.parse_args()
     main(args)
