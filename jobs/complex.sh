f200c738c3f70edd81ac5e6c81b5e24e979516a6
Daniel <dfdazac@gmail.com>
2023-01-24 14:11:19 +0100

    Update BioBLP-D jobs

--- jobs/complex.sh
+++ jobs/complex.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=bioblp-complex
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=01:00:00
-#SBATCH --mem=10G
-#SBATCH --partition=gpu_shared
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=05:00:00
+#SBATCH --mem=16G
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
 
 PROJ_FOLDER=bioblp
 OUT_FOLDER=models
@@ -17,18 +16,22 @@ cd $TMPDIR/$PROJ_FOLDER
 
 source activate bioblp
 
+git checkout disease-encoder
+
 python -m bioblp.train \
 --train_triples=data/biokgb/graph/biokg.links-train.csv \
 --valid_triples=data/biokgb/graph/biokg.links-valid.csv \
 --test_triples=data/biokgb/graph/biokg.links-test.csv \
---protein_data=data/biokgb/properties/protein_prottrans_embeddings.pt \
+--text_data=data/biokgb/properties/biokg_meshid_to_descr_name.tsv \
 --search_eval_batch_size=True \
---search_train_batch_size=True \
---dimension=1024 \
+--batch_size=64 \
+--num_negatives=512 \
+--learning_rate=2e-5 \
+--warmup_fraction=0.05 \
+--dimension=256 \
 --num_epochs=1 \
---batch_size=128 \
 --log_wandb=True \
---notes="BioBLP-prot search batch sizes"
+--notes="BioBLP-D 1 epoch test with in-batch negatives"
 
 # Keep files generated during job
 RESULTS_FOLDER=$HOME/$PROJ_FOLDER-$OUT_FOLDER
