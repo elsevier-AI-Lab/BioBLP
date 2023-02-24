2c27449428e79d65606f79a34d1313988126e700
Thom Pijnenburg <t.pijnenburg@elsevier.com>
2023-02-24 11:26:59 +0000

    Move benchmark encoders to separate file

--- bioblp/benchmarking/featurise.py
+++ bioblp/benchmarking/featurise.py
@@ -20,305 +20,22 @@ from tqdm import tqdm
 
 from typing import Tuple, List, Dict
 
-from bioblp.data import COL_EDGE, COL_SOURCE, COL_TARGET
 from bioblp.logging import get_logger
+from bioblp.benchmarking.utils import load_toml, ConfigJSONEncoder
+from bioblp.benchmarking.encoders import get_encoder
+from bioblp.benchmarking.encoders import MissingValueMethod
+from bioblp.benchmarking.encoders import EntityPairEncoder
+from bioblp.benchmarking.encoders import EntityEncoder
+from bioblp.benchmarking.encoders import NoiseEncoder
+from bioblp.benchmarking.encoders import StructuralPairEncoder
+from bioblp.benchmarking.encoders import RandomNoisePairEncoder
+from bioblp.benchmarking.encoders import KGEMPairEncoder
+
+from bioblp.data import COL_EDGE, COL_SOURCE, COL_TARGET
+from bioblp.benchmarking.encoders import ROTATE, TRANSE, COMPLEX, STRUCTURAL, NOISE, LABEL
 
 
 logger = get_logger(__name__)
-
-
-#
-# Constants
-#
-
-
-NOISE = "noise"
-STRUCTURAL = "structural"
-COMPLEX = "complex"
-TRANSE = "transe"
-ROTATE = "rotate"
-LABEL = "label"
-
-
-class MissingValueMethod(Enum):
-    DROP = "drop"
-    MEAN = "mean"
-    RANDOM = "random"
-
-
-#
-# Helpers
-#
-
-
-def get_random_tensor(shape, random_seed: int = 42, emb_range=(-1, 1)) -> Tensor:
-    r1 = emb_range[0]
-    r2 = emb_range[1]
-
-    g = torch.Generator()
-    g.manual_seed(random_seed)
-
-    return (r1 - r2) * torch.rand(shape, generator=g) + r2
-
-
-def load_toml(toml_path: str) -> dict:
-    toml_path = Path(toml_path)
-    config = {}
-    with open(toml_path, "r") as f:
-        config = toml.load(f)
-
-    return config
-
-
-class FeatureConfigJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        # add conditional logic for any data structures that require special care
-        # handling serialisation of Enum objects
-        if isinstance(obj, Path):
-            return str(obj.resolve())
-        return json.JSONEncoder.default(self, obj)
-
-#
-# Entity encoders to encode single entity type
-#
-
-
-class EntityEncoder(ABC):
-    """Written to resemble"""
-
-    def __init__(self, embeddings: Tensor, entity_to_id: dict):
-
-        self.embeddings = embeddings
-        self.entity_to_id = entity_to_id
-
-        self._emb_dim = self.embeddings.size(1)
-        self._emb_mean = torch.mean(self.embeddings, dim=0)
-        self._missing_value_cache = {}
-
-    def impute_missing_value(self, x, missing_value: MissingValueMethod) -> Tensor:
-        """can be {'drop', 'random', 'mean'}
-        """
-        emb = None
-
-        if missing_value is MissingValueMethod.DROP:
-            emb = torch.ones((self._emb_dim)) * float('-inf')
-
-        elif missing_value is MissingValueMethod.RANDOM:
-            if x in self._missing_value_cache:
-                emb = self._missing_value_cache.get(x)
-            else:
-                # create new random
-                # TODO: ensure distribution is similar to base embedding
-                random_perturbation = get_random_tensor((self._emb_dim))
-                emb = self._emb_mean + random_perturbation
-
-                # store in cache
-                self._missing_value_cache[x] = emb
-
-        elif missing_value is MissingValueMethod.MEAN:
-            emb = self._emb_mean
-
-        return emb
-
-    def encode(self, x: List[str], missing_value: str = MissingValueMethod.MEAN) -> Tuple[Tensor, Tensor]:
-        embs = []
-        mask = []
-
-        for idx, x_i in tqdm(enumerate(x), total=len(x)):
-            if x_i in self.entity_to_id:
-                # add to positive mask
-                mask.append(idx)
-
-                # collect embedding
-                emb_i = self.embeddings[self.entity_to_id.get(x_i)]
-                embs.append(emb_i)
-            else:
-                # return imputation strategy
-                embs.append(self.impute_missing_value(
-                    x_i, missing_value=missing_value))
-
-        embs = torch.stack(embs)
-        mask = torch.tensor(mask)
-        return embs, mask
-
-
-class NoiseEncoder(EntityEncoder):
-
-    @classmethod
-    def from_entities(cls, entities: List[str], random_seed: int = 42, dim: int = 128, emb_range: Tuple[int, int] = (-1, 1)):
-        entity_set = set(entities)
-        entity2id = {k: idx for idx, k in enumerate(sorted(list(entity_set)))}
-
-        emb_shape = (len(entities), dim)
-
-        embs = get_random_tensor(
-            shape=emb_shape, random_seed=random_seed, emb_range=emb_range)
-
-        return cls(embeddings=embs, entity_to_id=entity2id)
-
-
-class ProteinEncoder(EntityEncoder):
-
-    @classmethod
-    def from_file(cls, emb_file: Path, entity_to_id: Path):
-        logger.info(f"Loading protein data from {emb_file}...")
-
-        embs = torch.load(emb_file)
-        entity2id = {}
-
-        with open(entity_to_id, "r") as f:
-            entity2id = json.load(f)
-
-        entity2id = {v: int(k) for k, v in entity2id.items()}
-        return cls(embeddings=embs, entity_to_id=entity2id)
-
-
-class MoleculeEncoder(EntityEncoder):
-
-    @classmethod
-    def from_file(cls, emb_file: Path):
-        logger.info(f"Loading molecule data from {emb_file}...")
-        data_dict = torch.load(emb_file)
-
-        emb_data = data_dict['embeddings']
-        mol_ids = list(emb_data.keys())
-
-        entity2row = {mol_i: idx for idx, mol_i in enumerate(mol_ids)}
-        emb_records = [torch.from_numpy(
-            np.mean(emb_data.get(x), axis=0)) for x in mol_ids]
-
-        embs = torch.stack(emb_records, dim=0)
-
-        logger.info(embs.shape)
-
-        return cls(embeddings=embs, entity_to_id=entity2row)
-
-
-class KGEMEncoder(EntityEncoder):
-
-    @classmethod
-    def from_model(cls, model_file: Path, entity_to_id_file: Path, device: str = "cpu", filter_entities: List[str] = None):
-        logger.info(f"Loading model data from {model_file}...")
-
-        model = torch.load(model_file, map_location=torch.device(device))
-        entity2id_df = pd.read_csv(entity_to_id_file, sep="\t", header=0, compression="gzip")\
-            .sort_values(by="id", ascending=True)
-
-        entity_to_kgid = {k: v for (k, v) in entity2id_df[[
-            "label", "id"]].values}
-        logger.info(len(entity_to_kgid))
-
-        entity_kge_ids = []
-        entity_to_idx = {}
-
-        if filter_entities is not None:
-            entity_kge_ids = []
-            entity_to_idx = {}
-
-            for ent_idx, ent_i in enumerate(sorted(filter_entities)):
-                entity_kge_ids.append(entity_to_kgid.get(ent_i))
-                entity_to_idx[ent_i] = ent_idx
-
-            entity_idxs = torch.LongTensor(entity_kge_ids)
-
-        else:
-            entity_idxs = torch.LongTensor(entity2id_df["id"].values)
-            entity_to_idx = entity_to_kgid
-
-        embs = model.entity_representations[0]._embeddings(entity_idxs)
-
-        logger.info(f"Loaded embeddings with shape: {embs.shape}")
-
-        return cls(embeddings=embs, entity_to_id=entity_to_idx)
-
-
-#
-# Entity pair encoders
-#
-
-
-class EntityPairEncoder():
-    def __init__(self, left_encoder: EntityEncoder, right_encoder: EntityEncoder):
-
-        self.left_encoder = left_encoder
-        self.right_encoder = right_encoder
-
-    def _combine(self, left_enc: Tensor, right_enc: Tensor, transform: str = "concat"):
-        if transform == "concat":
-            return torch.cat([left_enc, right_enc], dim=1)
-        # default
-        return torch.cat([left_enc, right_enc], dim=1)
-
-    def encode(self, x: np.array, missing_value: MissingValueMethod = MissingValueMethod.DROP, transform: str = "concat") -> Tensor:
-        """
-        """
-        left = x[:, 0]
-        right = x[:, 1]
-
-        left_enc, left_mask = self.left_encoder.encode(
-            left, missing_value=missing_value)
-        right_enc, right_mask = self.right_encoder.encode(
-            right, missing_value=missing_value)
-
-        total_enc = self._combine(left_enc, right_enc, transform)
-        common_mask = torch.from_numpy(np.intersect1d(left_mask, right_mask))
-        return total_enc, common_mask
-
-
-def RandomNoisePairEncoder(entities: List[str], random_seed: int) -> EntityPairEncoder:
-
-    # prepare noisy embeddings
-    noise_encoder = NoiseEncoder.from_entities(
-        entities=entities, random_seed=random_seed)
-
-    pair_encoder = EntityPairEncoder(
-        left_encoder=noise_encoder, right_encoder=noise_encoder)
-    return pair_encoder
-
-
-def StructuralPairEncoder(proteins: Path, molecules: Path) -> EntityPairEncoder:
-
-    # prepare molecular embeddings
-    mol_path = Path(molecules)
-    mol_encoder = MoleculeEncoder.from_file(
-        emb_file=mol_path.joinpath("biokg_molecule_embeddings.pt"))
-
-    # prepare protein embeddings
-    proteins_path = Path(proteins)
-    prot_encoder = ProteinEncoder.from_file(
-        emb_file=proteins_path.joinpath("protein_embeddings_full_24_12.pt"),
-        entity_to_id=proteins_path.joinpath("biokg_uniprot_complete_ids.json"))
-
-    pair_encoder = EntityPairEncoder(
-        left_encoder=mol_encoder, right_encoder=prot_encoder)
-    return pair_encoder
-
-
-def KGEMPairEncoder(model_dir: Path, device: str = "cpu", entities: List[str] = None) -> EntityPairEncoder:
-    model_dir = Path(model_dir)
-    model_file = model_dir.joinpath("trained_model.pkl")
-
-    entity_to_id_file = model_dir.joinpath(
-        "training_triples/entity_to_id.tsv.gz")
-
-    kgem_encoder = KGEMEncoder.from_model(model_file=model_file,
-                                          entity_to_id_file=entity_to_id_file,
-                                          device=device,
-                                          filter_entities=entities)
-
-    pair_encoder = EntityPairEncoder(
-        left_encoder=kgem_encoder, right_encoder=kgem_encoder)
-
-    return pair_encoder
-
-
-def get_encoder(encoder_label: str, encoder_args: Dict[str, dict], entities: List[str]):
-    if encoder_label == NOISE:
-        return RandomNoisePairEncoder(entities=entities, **encoder_args)
-    elif encoder_label == STRUCTURAL:
-        return StructuralPairEncoder(**encoder_args)
-    elif encoder_label in {COMPLEX, TRANSE, ROTATE}:
-        return KGEMPairEncoder(**encoder_args)
 
 
 #
@@ -358,15 +75,17 @@ def save_features(outdir: Path, label: str, feature: Tensor, labels: Tensor):
     torch.save(torch_obj, outfile)
 
 
-def build_encodings(config: FeatureConfig, pairs: np.array, encoders: List[str], 
+def build_encodings(config: FeatureConfig, pairs: np.array, encoders: List[str],
                     encoder_args: Dict[str, dict], entities_filter: List[str]) -> Tuple[str, Tensor, Tensor]:
     encoded_bm = []
 
     for encoder_i_label in tqdm(encoders, desc=f"Encoding benchmarks..."):
         logger.info(f"Encoding with {encoder_i_label}")
         encoder_i_args = encoder_args.get(encoder_i_label)
-        pair_encoder = get_encoder(
-            encoder_i_label, encoder_i_args, entities=entities_filter)
+
+        pair_encoder = get_encoder(encoder_i_label,
+                                   encoder_i_args,
+                                   entities=entities_filter)
 
         missing_value_method = MissingValueMethod(config.missing_values)
 
@@ -396,7 +115,7 @@ def apply_common_mask(encoded_bm: List[Tuple[str, Tensor, Tensor]], labels: Tens
     return masked_encoded_bm, masked_labels
 
 
-def main(bm_file: str, conf: str, override_data_root=None, override_timestamp=None):
+def main(bm_file: str, conf: str, override_data_root=None, override_run_id=None):
 
     config = parse_feature_conf(conf)
 
@@ -405,9 +124,9 @@ def main(bm_file: str, conf: str, override_data_root=None, override_timestamp=No
 
     config = FeatureConfig(**config)
 
-    timestamp = override_timestamp or str(int(time()))
+    run_id = override_run_id or str(int(time()))
     logger.info(
-        f"Running process with config: {config} at time {timestamp}...")
+        f"Running process with config: {config} at time {run_id}...")
 
     # load benchmark data
     # here entities are strings
@@ -421,15 +140,19 @@ def main(bm_file: str, conf: str, override_data_root=None, override_timestamp=No
     labels = torch.from_numpy(bm_df[LABEL].values)
 
     # perform encodings
-    encoded_bm = build_encodings(config=config, pairs=pairs, encoders=config.encoders,
-                                 encoder_args=config.encoder_args, entities_filter=all_entities)
+    encoded_bm = build_encodings(config=config,
+                                 pairs=pairs,
+                                 encoders=config.encoders,
+                                 encoder_args=config.encoder_args,
+                                 entities_filter=all_entities)
 
     # add plain benchmark data too
     encoded_bm.append(("raw", pairs, np.arange(len(pairs))))
 
     # common mask only when dropping missing embeddings
     if config.missing_values == MissingValueMethod.DROP.value:
-        masked_encoded_bm, masked_labels = apply_common_mask(encoded_bm, labels)
+        masked_encoded_bm, masked_labels = apply_common_mask(
+            encoded_bm, labels)
     else:
         masked_encoded_bm = [(x[0], x[1]) for x in encoded_bm]
         masked_labels = labels
@@ -437,7 +160,7 @@ def main(bm_file: str, conf: str, override_data_root=None, override_timestamp=No
     logger.info("Saving features...")
 
     feature_outdir = Path(config.data_root).joinpath(
-        config.experiment_root).joinpath(timestamp)
+        config.experiment_root).joinpath(run_id)
     feature_outdir.mkdir(parents=True, exist_ok=True)
 
     for enc_label, enc_pairs in masked_encoded_bm:
@@ -450,7 +173,8 @@ def main(bm_file: str, conf: str, override_data_root=None, override_timestamp=No
 
     with open(feature_outdir.joinpath("config.json"), "w") as f:
         cfg_dict = asdict(config)
-        json.dump(cfg_dict, f, cls=FeatureConfigJSONEncoder)
+        json.dump(cfg_dict, f, cls=ConfigJSONEncoder)
+
 
 def get_parser() -> ArgumentParser:
     parser = ArgumentParser(
@@ -465,8 +189,8 @@ def get_parser() -> ArgumentParser:
     # parser.add_argument("--rotate", type=str, help="Path to rotate data")
     parser.add_argument("--override_data_root", type=str,
                         help="Path to root of data tree")
-    parser.add_argument("--override_timestamp", type=str,
-                        help="Path to root of data tree")
+    parser.add_argument("--override_run_id", type=str,
+                        help="Override run_id")
 
     return parser
 
