dec6c34c6e60d2ef52898c09adac574a0177fe24
Daniel <dfdazac@gmail.com>
2023-03-14 17:26:04 +0100

    Update BioBLP-M jobs

---
+++ jobs/biokg-bioblp-m-rotate-sweep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=biokg-rotate-sweep
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=72:00:00
+#SBATCH --mem=16G
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+
+PROJ_FOLDER=bioblp
+OUT_FOLDER=models
+
+# Copy data to scratch
+cp -r $HOME/$PROJ_FOLDER $TMPDIR
+cd $TMPDIR/$PROJ_FOLDER
+
+source activate bioblp
+
+git checkout freeze-embeddings
+wandb agent --count 1 discoverylab/bioblp/3gvh1mq2
+
+# Keep files generated during job
+RESULTS_FOLDER=$HOME/$PROJ_FOLDER-$OUT_FOLDER
+mkdir -p $RESULTS_FOLDER
+cp -r $TMPDIR/$PROJ_FOLDER/$OUT_FOLDER/* $RESULTS_FOLDER
