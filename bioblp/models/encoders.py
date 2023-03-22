a3abe7fa4c94b32268da612d68cee680408ff382
p.mitra <p.mitra@elsevier.com>
2023-03-22 00:56:25 +0000

    resolve tuple data struct discrepancy with python3.9 upgrade

--- bioblp/models/encoders.py
+++ bioblp/models/encoders.py
@@ -6,7 +6,6 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
-from typing import Tuple
 
 from ..loaders.preprocessors import (TextEntityPropertyPreprocessor,
                                      MolecularFingerprintPreprocessor,
