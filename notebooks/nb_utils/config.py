c6c856817ec289a2ac4524b16c59945509e822c3
p.mitra <p.mitra@elsevier.com>
2023-04-03 21:44:23 +0000

    add artifactory registry and utils

---
+++ notebooks/nb_utils/config.py
@@ -0,0 +1,152 @@
+import abc
+import toml
+import json
+
+from enum import Enum
+from dataclasses import dataclass, field
+from typing import List, Dict
+import pandas as pd
+from pathlib import Path
+from pykeen.triples import TriplesFactory
+
+
+DISEASE = 'disease'
+MOLECULE = 'molecule'
+PROTEIN = 'protein'
+
+
+ARTIFACT_REGISTRY_TOML_PATH = "/home/jovyan/BioBLP/notebooks/nb_utils/artifact_registry.toml"
+
+
+class EntityType(str, Enum):
+    disease = DISEASE
+    molecule = MOLECULE
+    protein = PROTEIN
+
+
+def load_toml(toml_path: str) -> dict:
+    toml_path = Path(toml_path)
+    config = {}
+    with open(toml_path, "r") as f:
+        config = toml.load(f)
+
+    return config
+
+
+@dataclass
+class ModelRegistryConfig:
+    #model_base_dir: str
+    registered_model_list: List[str]
+    registered_model_paths: Dict
+    registered_model_training_triples_paths: Dict
+    
+    @classmethod
+    def from_toml(cls, toml_path):
+    
+        def cfg_from_dict(cfg_toml_dict):
+            model_root_dir = Path(cfg_toml_dict.get("model_registry_root_dir"))
+            registered_model_list = list(cfg_toml_dict.get("kge_models").get("registered_models"))
+            registered_model_paths = {v: model_root_dir.joinpath(f"{v}") for v in registered_model_list}
+            registered_model_training_triples_paths = {k: v.joinpath("training_triples") for k,v in registered_model_paths.items()}
+            cfg = {}
+            cfg.update({"registered_model_list": registered_model_list})
+            cfg.update({"registered_model_paths": registered_model_paths})
+            cfg.update({"registered_model_training_triples_paths": registered_model_training_triples_paths})
+            return cfg
+        
+        cfg_toml_dict = load_toml(toml_path)
+        cfg = cfg_from_dict(cfg_toml_dict)
+
+        return cls(**cfg)
+
+
+# todo: delete
+@dataclass
+class GraphRegistryConfig:
+    graph_root_dir = str
+    biokgb_data_splits: Dict
+    biokgb_entity_type_metadata_paths: Dict
+    biokgb_entity_attribute_paths: Dict
+        
+    @classmethod
+    def from_toml(cls, toml_path):
+        cfg_toml_dict = load_toml(toml_path)
+
+        biokgb_graph_cfg = cfg_toml_dict.get("biokgb_graph")
+        biokgb_data_root_dir = Path(biokgb_graph_cfg.get("biokgb_data_root_dir"))
+        biokgb_graph_dir = biokgb_data_root_dir.joinpath(biokgb_graph_cfg.get("graph_dir"))
+        biokgb_properties_dir = biokgb_data_root_dir.joinpath(biokgb_graph_cfg.get("properties_dir"))
+        biokgb_properties = biokgb_graph_cfg.get("properties")
+        
+        biokgb_data_splits = {k: biokgb_graph_dir.joinpath(v) for k,v in biokgb_graph_cfg.get("data_splits").items()}
+        
+        biokgb_entity_type_metadata_paths = {k: biokgb_properties_dir.joinpath(v) for k,v in biokgb_properties.get("entity_type_metadata_paths").items()}
+        biokgb_entity_attribute_paths = {k: biokgb_properties_dir.joinpath(v) for k,v in biokgb_properties.get("entity_attribute_paths").items()}
+        
+        cfg = {}
+        cfg.update({"biokgb_data_splits": biokgb_data_splits})
+        cfg.update({"biokgb_entity_type_metadata_paths": biokgb_entity_type_metadata_paths})
+        cfg.update({"biokgb_entity_attribute_paths": biokgb_entity_attribute_paths})
+        return cls(**cfg)
+    
+
+    
+@dataclass
+class EvaluationConfig:
+    model_name: str
+    model_cfg: Dict
+    test_set_list: List[str]
+    test_ent_w_attribute_of_type: Enum
+    
+    def get_model_config(self, artifact_toml_path=ARTIFACT_REGISTRY_TOML_PATH):
+        model_registry_cfg = ModelRegistryConfig.from_toml(artifact_toml_path)
+        registered_models = model_registry_cfg.registered_model_list
+        if self.model_name not in registered_models:
+            raise ValueError(f"{self.model_name} not in {registered_models}")
+        
+        model_path = model_registry_cfg.registered_model_paths[self.model_name]
+        training_triples_path = model_path.joinpath('training_triples')
+
+        self.model_cfg.update({'model_name': self.model_name})
+        self.model_cfg.update({'model_path': model_path})
+        self.model_cfg.update({'training_triples_path': training_triples_path})       
+        return self
+    
+    def update_test_set_dict(self, test_set_list):
+        if not test_set_triples:
+            test_set_list = default_test_set_list()    
+        self.test_set_list = test_set_list
+        
+        
+def get_default_biokg_test_sets(
+    artifact_registry_toml_path=ARTIFACT_REGISTRY_TOML_PATH
+):
+    toml_cfg = load_toml(artifact_registry_toml_path)
+    default_test_set_list = toml_cfg.get('test_set_lists')
+    return default_test_set_list
+
+
+def create_entity_attr_aware_test_sets(entity_type_w_attribute: str, 
+                                       graph_cfg: GraphRegistryConfig,
+                                       train: TriplesFactory, 
+                                       test: TriplesFactory):
+    entity_metadata_path = graph_cfg.biokgb_entity_type_metadata_paths.get(entity_type_w_attribute)
+    entity_attribute_path = graph_cfg.biokgb_entity_attribute_paths.get(entity_type_w_attribute)
+    
+    # create a subset of biokg entities of type Protein 
+    entities = pd.read_csv(entity_metadata_path, sep="\t", names=[entity_type_w_attribute, "rel", "node_type"])
+    entity_set = set(entities[entity_type_w_attribute].values)
+    print(f"# {entity_type_w_attribute} entities in larger biokg (pre-benchmark removal): {len(entity_set)}")
+
+    # create a set of protein entities for which we have text descriptions
+    entity_w_attr_df = pd.read_json(entity_attribute_path, orient="index").reset_index()#, sep="\t", header=0, names=["protein", "attr"])
+    entity_w_attr_df.rename(columns={"index": entity_type_w_attribute, 0: "attr"}, inplace=True)
+    entity_attr_set = set(entity_w_attr_df[entity_type_w_attribute].values)
+    print(f"# {entity_type_w_attribute} entities for which we have attributes: {len(entity_attr_set)}")
+    
+    test_triples_incl_ent_prop, test_triples_excl_ent_prop = split_test_triples_conditioned_on_ent_property(train_triples=train, 
+                                                                                                            typed_ent_set=entity_set, 
+                                                                                                            typed_ent_with_prop_set=entity_attr_set,
+                                                                                                            test_triples=test)
+
+    return test_triples_incl_ent_prop, test_triples_excl_ent_prop
--- notebooks/nb_utils/eval_utils.py
+++ notebooks/nb_utils/eval_utils.py
@@ -1,8 +1,17 @@
+from abc import ABC
+from enum import Enum
+import matplotlib.pyplot as plt
+from .config import GraphRegistryConfig
+import numpy as np
 import pandas as pd
+from pathlib import Path
 from pykeen.triples import TriplesFactory
 from pykeen.evaluation import RankBasedEvaluator
+import seaborn as sns
+import torch
 from tqdm import tqdm
 import wandb
+import time
 
 ###### Define CONSTANTS ######
 TEST = "test"
@@ -11,6 +20,9 @@ TRAIN = "train"
 TEST_RESTRICTED_DIS = 'TEST_RESTRICTED_DIS'
 TEST_EXCLUDING_DIS = 'TEST_EXCLUDING_DIS'
 VALID_RESTRICTED_DIS = 'VALID_RESTRICTED_DIS'
+TEST_RESTRICTED_PROT = 'TEST_RESTRICTED_PROT'
+TEST_EXCLUDING_PROT = 'TEST_EXCLUDING_PROT'
+
 
 COL_SOURCE = "src"
 COL_TARGET = "tgt"
@@ -28,11 +40,52 @@ WANDB_ENTITY_DISCOVERYLAB = "discoverylab"
 EVAL_NODE_BOTH = "both"
 EVAL_NODE_HEAD = "head"
 EVAL_NODE_TAIL = "tail"
-EVAL_METRIC_REALISTIC = "realistic"
-EVAL_METRICS_SHORTLIST = ["arithmetic_mean_rank", "adjusted_arithmetic_mean_rank", 
-                          "inverse_harmonic_mean_rank", "hits_at_1",
-                          "hits_at_3", "hits_at_5", "hits_at_10"] 
 
+EVAL_METRIC_REALISTIC = "realistic"
+EVAL_METRIC_OPTIMISTIC = "optimistic"
+EVAL_METRIC_PESSIMISTIC = "pessimistic"
+EVAL_METRIC_RANKING_TYPES = {ranking_type: ranking_type for ranking_type in 
+                             [EVAL_METRIC_OPTIMISTIC,EVAL_METRIC_PESSIMISTIC, EVAL_METRIC_REALISTIC]
+                            }
+
+HITS_AT_1 = "hits_at_1"
+HITS_AT_3 = "hits_at_3"
+HITS_AT_5 = "hits_at_5"
+HITS_AT_10 = "hits_at_10"
+ARITHMETIC_MEAN_RANK = "arithmetic_mean_rank"
+ADJUSTED_ARITHMETIC_MEAN_RANK = "adjusted_arithmetic_mean_rank"
+INVERSE_HARMONIC_MEAN_RANK = "inverse_harmonic_mean_rank"
+
+EVAL_METRICS_CATALOG = {
+    ARITHMETIC_MEAN_RANK: ARITHMETIC_MEAN_RANK,
+    ADJUSTED_ARITHMETIC_MEAN_RANK: ADJUSTED_ARITHMETIC_MEAN_RANK,
+    INVERSE_HARMONIC_MEAN_RANK: INVERSE_HARMONIC_MEAN_RANK,
+    HITS_AT_1: HITS_AT_1,
+    HITS_AT_3: HITS_AT_3,
+    HITS_AT_5: HITS_AT_5,
+    HITS_AT_10: HITS_AT_10
+}
+
+EVAL_METRICS_SHORTLIST = [HITS_AT_1, HITS_AT_3, 
+                          HITS_AT_5, HITS_AT_10, 
+                          INVERSE_HARMONIC_MEAN_RANK] 
+
+
+class EvalMetricRankingType(Enum):
+    optimistic = EVAL_METRIC_OPTIMISTIC
+    pessimistic = EVAL_METRIC_PESSIMISTIC
+    realistic = EVAL_METRIC_REALISTIC
+    
+
+def load_kge_model(model_base_path):
+    '''model = load_kge_model(model_base_path = model_registry_paths[MODEL_ID])
+    '''
+    model_path = model_base_path.joinpath("trained_model.pkl")
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = torch.load(model_path, map_location=torch.device(device))
+    #train = TriplesFactory.from_path_binary(model_base_path.joinpath("training_triples"))
+    return model
+  
 
 ###### WandB utils ###### 
 
@@ -74,11 +127,53 @@ def run_experiment_and_log_wandb(study_name:str,
 
 ##### evaluation logic ##### 
 
+
+class LPEvaluator(ABC):
+    '''
+    Does not add too much. Do away with.
+    This LP Evaluator currently expects KGE models with pykeen API
+    for eval_metric_ranking_type options, see: 
+    https://pykeen.readthedocs.io/en/stable/tutorial/understanding_evaluation.html#ranking-types
+    '''
+    def __init__(self, 
+                 eval_metric_ranking_type=None):
+        
+        if not eval_metric_ranking_type or eval_metric_ranking_type not in EVAL_METRIC_RANKING_TYPES.keys():
+            eval_metric_ranking_type=EVAL_METRIC_REALISTIC
+        
+        self.eval_metric_ranking_type=eval_metric_realistic_type
+        
+        
+    def evaluate_lp_all_rels_on_single_test_set(self,
+                                                model, 
+                                                eval_test_set_slug,
+                                                train_triples=None,
+                                                test_triples=None,
+                                                valid_triples=None,
+                                                eval_triple_node_endpoint=EVAL_NODE_BOTH):
+
+        results_all_rels_dict = evaluate_lp_all_rels_on_single_test_set(model, 
+                                                                        eval_test_set_slug=eval_test_set_slug,
+                                                                        eval_triples=eval_triples,
+                                                                        train_triples=train_triples,
+                                                                        valid_triples=valid_triples,
+                                                                        eval_triple_node_endpoint=EVAL_NODE_BOTH)
+        # add to report/save to file?
+        return results_all_rels_dict
+        
+        
+    def evaluate_lp_relwise_on_single_test_set(self,
+                                               **kwargs):
+        results_relwise_dict = evaluate_lp_relwise_on_single_test_set(**kwargs)
+        return results_relwise_dict
+        
+    
 def evaluate_lp_all_rels_on_single_test_set(model, 
                                             eval_test_set_slug,
                                             eval_triples=None,
                                             train_triples=None,
-                                            valid_triples=None,                                            
+                                            valid_triples=None, 
+                                            eval_triple_node_endpoint=EVAL_NODE_BOTH
                                            ):
             
     evaluator = RankBasedEvaluator(filtered=True)   
@@ -91,8 +186,10 @@ def evaluate_lp_all_rels_on_single_test_set(model,
                                      additional_filter_triples = filtered_triples)
     
     results_all_rels = make_results_dict_all_rel(eval_result, 
-                                         relation='All', 
-                                         relation_count=eval_triples.num_triples)
+                                                 relation='All', 
+                                                 relation_count=eval_triples.num_triples,
+                                                 triple_endpoint=eval_triple_node_endpoint
+                                                )
     return results_all_rels
 
 
@@ -100,7 +197,8 @@ def evaluate_lp_relwise_on_single_test_set(model,
                                             eval_test_set_slug,
                                             eval_triples=None,
                                             train_triples=None,
-                                            valid_triples=None,                                              
+                                            valid_triples=None,    
+                                           eval_triple_node_endpoint=EVAL_NODE_BOTH
                                            ):
     evaluator = RankBasedEvaluator(filtered=True)   
     result_dicts =[]
@@ -117,7 +215,8 @@ def evaluate_lp_relwise_on_single_test_set(model,
                                                additional_filter_triples=additional_filter_triples,
                                                use_tqdm=False)
             result_dicts.append({'results': subset_result, 'relation': relation, 'count': triples_subset.num_triples})
-    results_df = pd.DataFrame([make_results_dict_all_rel(d['results'], d['relation'], d['count']) for d in result_dicts])
+    results_df = pd.DataFrame([make_results_dict_all_rel(d['results'], d['relation'], d['count']) for d in result_dicts],
+                             triple_endpoint=eval_triple_node_endpoint)
     rel_results_dict = results_df.set_index('Relation').transpose().to_dict()
     return rel_results_dict
 
@@ -140,8 +239,8 @@ def make_results_dict_all_rel(results, relation, relation_count,
     return results_dict
 
 def make_results_dict_rel_breakdown(results, relation, relation_count,
-                                    triple_endpoint="both",
-                                    metric_type = "realistic",
+                                    triple_endpoint=EVAL_NODE_BOTH,
+                                    metric_type = EVAL_METRIC_REALISTIC,
                                     metrics_shortlist=EVAL_METRICS_SHORTLIST):
     if not relation:
         relation= 'All'
@@ -155,6 +254,33 @@ def make_results_dict_rel_breakdown(results, relation, relation_count,
 
 
 ##### test set processing stuff #####
+
+
+def create_entity_attr_aware_test_sets(entity_type_w_attribute: str, 
+                                       graph_cfg: GraphRegistryConfig,
+                                       train: TriplesFactory, 
+                                       test: TriplesFactory):
+    entity_metadata_path = graph_cfg.biokgb_entity_type_metadata_paths.get(entity_type_w_attribute)
+    entity_attribute_path = graph_cfg.biokgb_entity_attribute_paths.get(entity_type_w_attribute)
+    
+    # create a subset of biokg entities of type Protein 
+    entities = pd.read_csv(entity_metadata_path, sep="\t", names=[entity_type_w_attribute, "rel", "node_type"])
+    entity_set = set(entities[entity_type_w_attribute].values)
+    print(f"# {entity_type_w_attribute} entities in larger biokg (pre-benchmark removal): {len(entity_set)}")
+
+    # create a set of protein entities for which we have text descriptions
+    entity_w_attr_df = pd.read_json(entity_attribute_path, orient="index").reset_index()#, sep="\t", header=0, names=["protein", "attr"])
+    entity_w_attr_df.rename(columns={"index": entity_type_w_attribute, 0: "attr"}, inplace=True)
+    entity_attr_set = set(entity_w_attr_df[entity_type_w_attribute].values)
+    print(f"# {entity_type_w_attribute} entities for which we have attributes: {len(entity_attr_set)}")
+    
+    test_triples_incl_ent_prop, test_triples_excl_ent_prop = split_test_triples_conditioned_on_ent_property(train_triples=train, 
+                                                                                                            typed_ent_set=entity_set, 
+                                                                                                            typed_ent_with_prop_set=entity_attr_set,
+                                                                                                            test_triples=test)
+
+    return test_triples_incl_ent_prop, test_triples_excl_ent_prop
+
 
 def split_train_ents_by_existance_of_properties(train_triples: TriplesFactory, 
                                                 typed_ent_set: set,
@@ -222,3 +348,5 @@ def compute_node_degrees_in_out(triple_df):
     node_degree_df[DEGREE] = node_degree_df[IN_DEGREE]+node_degree_df[OUT_DEGREE]
     node_degree_df = node_degree_df.fillna(0)
     return node_degree_df
+
+
